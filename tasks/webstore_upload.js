526301eb3559b03f904f5fa160be94214eefb9cc
Jakob Færch <jrf@trifork.com>
2016-03-10 10:27:59 +0100

    Make diff to base contain exactly what the pull request should contain - these lines were on my fork mysteriously

--- tasks/webstore_upload.js
+++ tasks/webstore_upload.js
@@ -64,16 +64,7 @@ module.exports = function (grunt) {
             accounts = grunt.config(accountsConfigPath);
             var extensionsToUpload = extensions;
             if(tasks.length){
-                //validate extension name
-                _.each(tasks, function(task){
-                    if( !extensionsToUpload[task] ){
-                        var msg = 'Configuration for "' +
-                            task
-                            + '" not exist, please check configuration of the extensions list';
-                        grunt.fail.warn(msg);
-                    }
-                });
-                extensionsToUpload = _.pick(extensions, tasks);
+            	extensionsToUpload = _.pick(extensions, tasks)
             }
 
             grunt.registerTask( 'get_account_token', 'Get token for account',
